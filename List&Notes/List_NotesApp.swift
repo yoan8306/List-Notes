9acbfdb0b982ebab52745b04d730c092ddfc1e03
yoan8306 <34924156+yoan8306@users.noreply.github.com>
2021-04-18 20:43:33 +0200

    Initial Commit

---
+++ List&Notes/List_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  List_NotesApp.swift
+//  List&Notes
+//
+//  Created by Yoan on 18/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct List_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
